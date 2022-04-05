807d2405714ebd7e37b93fad4ac3634bf2490109
Joe Sitton <sittonjoe@gmail.com>
2022-04-04 23:27:39 -0400

    nice

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,6 @@ for _, module in ipairs(init_modules) do
   local ok, err = pcall(require, module)
 
   if not ok then
-    error("Error loading " .. module .. "\n\n" .. err)
+    print("Error loading " .. module .. "\n\n" .. err)
   end
 end
